a8f3eb46c3e9e0277f48f4f3159cd580b260a4fe
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-06-15 14:13:10 +0900

    기본적인 폼 추가

--- docs/components/CreateQrCode/CreateQrCode.tsx
+++ docs/components/CreateQrCode/CreateQrCode.tsx
@@ -1,6 +1,69 @@
 import { getPublicKey } from "@site/src/api";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import crypto from "crypto-js";
 
 export default function CreateQrCode(): JSX.Element {
-  return <>TEST</>;
+  const [publicKey, setPublicKey] = useState<string | null>(null);
+  const [ltuid, setLtuid] = useState<string>("");
+  const [ltoken, setLtoken] = useState<string>("");
+  const [cookietoken, setCookietoken] = useState<string>("");
+  const [data, setData] = useState<Record<string, string>>({});
+
+  useEffect(() => {
+    getPublicKey().then((key: string) => {
+      setPublicKey(key);
+    });
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <p>
+          <label>ltuid</label>
+          <input
+            type="text"
+            name="ltuid"
+            onChange={onChangeHandlerFactory(setLtuid)}
+          />
+        </p>
+        <p>
+          <label>ltoken</label>
+          <input
+            type="text"
+            name="ltoken"
+            onChange={onChangeHandlerFactory(setLtoken)}
+          />
+        </p>
+        <p>
+          <label>cookietoken</label>
+          <input
+            type="text"
+            name="cookietoken"
+            onChange={onChangeHandlerFactory(setCookietoken)}
+          />
+        </p>
+        <div>
+          <button
+            onClick={() => {
+              setData({
+                ltuid,
+                ltoken,
+                cookietoken,
+              });
+            }}
+          >
+            생성하기!
+          </button>
+        </div>
+      </form>
+      <pre>{JSON.stringify(data)}</pre>
+    </div>
+  );
 }
+
+const onChangeHandlerFactory =
+  (setValue) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+// const digestRSA = { publicKey, data };
